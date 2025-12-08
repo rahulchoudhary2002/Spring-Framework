fd0dd499597f1202a4356c9a38c833b5c3bbc38d
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-12-08 21:03:06 +0530

    autowired

---
+++ SpringCore/field-based-DI-using-autowired/src/main/java/jsp/springscoreDemo1/Mobile.java
@@ -0,0 +1,13 @@
+package jsp.springscoreDemo1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Mobile {
+	public void ring() {
+		System.out.println("mobile is ringing");
+	}
+}
+
+
+
