257c4f0aed8a1cb29e4d7d9cbd022e21fec8d0f2
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-12-15 23:38:30 +0530

    add spring mvc project

---
+++ SpringMVC/spring-mvc-demo/src/main/java/jsp/springmvc/MyConfiguration.java
@@ -0,0 +1,10 @@
+package jsp.springmvc;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="jsp.springmvc")
+public class MyConfiguration {
+
+}
---
+++ SpringMVC/spring-mvc-demo/src/main/java/jsp/springmvc/WebInitializer.java
@@ -0,0 +1,25 @@
+package jsp.springmvc;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		
+		return new Class[] {MyConfiguration.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+
+}
