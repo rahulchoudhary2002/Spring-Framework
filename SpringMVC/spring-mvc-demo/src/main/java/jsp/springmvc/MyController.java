869bc453e4ab17c6fb520cef167f43c18473bfcf
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-12-16 21:40:13 +0530

    Model and ModelMap

--- SpringMVC/spring-mvc-demo/src/main/java/jsp/springmvc/MyController.java
+++ SpringMVC/spring-mvc-demo/src/main/java/jsp/springmvc/MyController.java
@@ -1,13 +1,31 @@
 package jsp.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
 	@GetMapping("/home")
 	public String homePage() {
-		return("home.jsp");
+		return ("home.jsp");
+
+	}
+
+	@GetMapping("/student")
+	public String getStudent(Model model) {
+		model.addAttribute("sid", "12");
+		model.addAttribute("sname", "smith");
+		return "student.jsp";
+
+	}
+	@GetMapping("/employee")
+	public String getEmployee(ModelMap map){
+		map.put("ename","riya");
+		map.put("salary","42000");
+		
+		return "employee.jsp";
 		
 	}
 }
