490980eb205f8f07fdfc4a35e976c77358e3f28d
rahulchoudhary2002 <rahulchoudhary19march@gmail.com>
2025-12-17 22:43:56 +0530

    ModelAndView , form

--- SpringMVC/spring-mvc-demo/src/main/java/jsp/springmvc/MyController.java
+++ SpringMVC/spring-mvc-demo/src/main/java/jsp/springmvc/MyController.java
@@ -4,6 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -26,6 +31,34 @@ public class MyController {
 		map.put("salary","42000");
 		
 		return "employee.jsp";
+		
+	}
+	@GetMapping("/appliction")
+	public ModelAndView getApp(ModelAndView view) {
+		
+		view.addObject("appName","whatsapp");
+		view.addObject("appVersion","1.0");
+		view.setViewName("app.jsp");
+		return view;
+		
+	}
+	@GetMapping("/form")
+	public String triggerForm() {
+		return "form.jsp";
+		}
+	@PostMapping("/save")
+	public ModelAndView saveStudent(HttpServletRequest req,ModelAndView view) {
+		String sid=req.getParameter("id");
+		String sname=req.getParameter("name");
+		String sage=req.getParameter("age");
+		String sgender=req.getParameter("gender");
+		view.addObject("id",sid);
+		view.addObject("name",sname);
+		view.addObject("age",sage);
+		view.addObject("gender",sgender);
+		
+		view.setViewName("display.jsp");
+		return view;
 		
 	}
 }
---
+++ SpringMVC/spring-mvc-demo/src/main/webapp/form.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="save"   method="post">
+	 <label >ID:</label><br>
+        <input type="number" name="id"><br><br>
+
+        <label >Name:</label><br>
+        <input type="text"  name="name"><br><br>
+
+        <label >Age:</label><br>
+        <input type="number"  name="age"><br><br>
+        
+        <label> Gender</label>
+        <input type="text" name="gender">Gender 
+
+        <button>submit</button>
+	
+
+</form>
+</body>
+</html>
